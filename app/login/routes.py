04d3a608b1d05f8f10a45e01513e6cd869f98f28
FadliAktsar <muhammadfadliaktsar@gmail.com>
2024-01-25 08:27:43 +0700

    update alamat form di file login.html & bikin logic di routes login

--- app/login/routes.py
+++ app/login/routes.py
@@ -1,28 +1,14 @@
-from flask import render_template,request,flash, redirect, url_for
+from flask import render_template, redirect, url_for, request, flash, scalar_one_or_none
+from werkzeug.security import generate_password_hash
+from app.model.database.user import user
 from app.login import bp
+from app.extension import db
 
-@bp.route('/login', methods=['GET, POST'])
+@bp.route('/', methods=['GET', 'POST'])
 def login():
-    '''
-    if request.methods == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+   if request.method == 'POST':
+    username = request.method.get('username')
+    password = request.method.get('password')
 
-        users = db.session.execute(
-            db.select(users).filter_by(username=username)).scalar_one_or_none()
-        if users:
-            if check_password_hash(users.password, password):
-                flash('Login berhasil!', category='success')
-                login_user(users, remember=True)
-
-                existing_preferences = db.session.execute(
-                    db.select(Preference).filter(Preference.user_id=current_user.id)).scalars().all()
-                if existing_preferences:
-                    return redirect(url_for('main.index'))
-                retrun redirect(url_for('preference.choose_preference'))
-            else:
-                flash('Password salah, coba lagi!', category='error')
-        else:
-            flash('Username tidak terdaftar!', category='error')
-    '''
+    excisting_user = db.session.execute(db.select(user).filter_by(username=username)),scalar_one_or_none()
     return render_template('auth/login.html')
