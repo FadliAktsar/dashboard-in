5917c4b022ecb118c75313f56e762060de55c0ec
Fadli Aktsar <muhammadfadliaktsar@gmail.com>
2024-02-26 13:32:59 +0700

    move notebook file and model file to new dir (dir: notebooks and models)

--- app/peramalan/routes.py
+++ app/peramalan/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, flash
 #from flask_login import login_required, current_user
 import pickle
+from abc import ABC, abstractclassmethod
 
 from app.peramalan import bp
 with open('app/arima_model.pkl', 'rb') as file:
@@ -15,9 +16,9 @@ def index():
             # Ambil data input dari formulir
             input_data = request.form.get('input_data')
             # Lakukan peramalan menggunakan model
-            hasil_ramalan = model.predict([input_data], '%Y-%m-%d')
+            hasil_ramalan = model.predict([input_data])
         except ValueError:
-            flash('Format salah data.')
+            flash('Error.')
         # Render template atau tampilkan hasil peramalan
         return render_template('hasil_ramalan.html', hasil_ramalan=hasil_ramalan)
  
