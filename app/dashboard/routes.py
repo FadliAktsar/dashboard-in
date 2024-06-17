b42260b4df7f24ccf290d13912193e752f5c4e43
Fadli Aktsar <muhammadfadliaktsar@gmail.com>
2024-06-17 20:13:48 +0700

    major update (add forecasting feature)

--- app/dashboard/routes.py
+++ app/dashboard/routes.py
@@ -1,35 +1,75 @@
-from flask import render_template, request, flash, jsonify
+from flask import render_template, jsonify, request
 from app.extension import db
 from app.dashboard import bp
 from sqlalchemy import func
 from app.models.transaksi import Transaksi
+import datetime
+import joblib
+import pandas as pd
+
+model = joblib.load('app/notebooks/model.joblib')
 
 @bp.route('/')
 #@login_required
 def index():
-
     return render_template('dashboard/index.html')
+
+def daterange(start_date, end_date):
+    for n in range(int((end_date-start_date).days)+1):
+        yield  start_date + datetime.timedelta(n)
 
 @bp.route('/get_data')
 def get_data():
     try:    
-    
+        #Earliest and Latest Transaction Dates
+        earliest_date = db.session.query(func.min(Transaksi.Settlement_Date)).scalar()
+        latest_date = db.session.query(func.max(Transaksi.Settlement_Date)).scalar()
+
+        if earliest_date is None or latest_date is None:
+            return jsonify({'data':[], 'labels':[]})
+
+        #DB Query
         transaction = db.session.execute(
             db.select(Transaksi.Settlement_Date,
                       func.sum(Transaksi.Amount).label("Revenue")).group_by(Transaksi.Settlement_Date).order_by(Transaksi.Settlement_Date)
         ).all()
+    
+        # Convert Query Result to a Dictionary
+        transaction_dict = {t[0]: t[1] for t in transaction}
 
         # Convert to list of dictionaries
-        labels = [t[0].strftime('%Y-%m-%d') for t in transaction]
-        data = [t[1] for t in transaction]
+        labels = []
+        data = []
 
-        return jsonify({'data': data, 'labels': labels})
+        for single_date in daterange(earliest_date, latest_date):
+            labels.append(single_date.strftime('%Y-%m-%d'))
+            if single_date in transaction_dict:
+                data.append(transaction_dict[single_date])
+            else:
+                data.append(0)
+        
+        # Handle forecast input
+        input_periods = int(request.args.get('periods', 30))  # Default forecast period is 30 days
+        forecast_range = pd.date_range(start=latest_date + datetime.timedelta(days=1),
+                                        periods=input_periods, freq='D')
+        
+        # Forecast using ARIMA model
+        transaction_series = pd.Series(data, index=pd.to_datetime(labels))
+        prediction = model.get_prediction(start=len(transaction_series), end=len(transaction_series) + input_periods - 1)
+        prediction_mean = prediction.predicted_mean
+
+        # Prepare forecast labels and data
+        forecast_labels = [date.strftime('%Y-%m-%d') for date in forecast_range]
+        forecast_data = prediction_mean.tolist()
+
+        return jsonify({
+            'data': data,
+              'labels': labels,
+                'forecast_labels': forecast_labels,
+                    'forecast_data': forecast_data
+                  })
     
     except Exception as e:
         error_text = "<p>The error:<br>" + str(e) + "</p>"
         hed = '<h1>Something is broken.</h1>'
         return hed + error_text
-
-@bp.route('/forecast')
-def forecast():
-    pass
--- app/static/js/canvas.js
+++ app/static/js/canvas.js
@@ -1,5 +1,3 @@
-var _labels = [];
-var _data = [];
 const clx = document.getElementById('line-chart').getContext('2d')
 $.ajax({
         url: "/dashboard/get_data",
@@ -8,17 +6,26 @@ $.ajax({
             console.log("Response Data:", response);
             _data = response.data;
             _labels = response.labels;
+            _forecast_data = response.forecast_data;
+            _forecast_labels = response.forecast_labels;
 
             new Chart(clx, {
                 type: 'line',
                 data: {
-                    labels: _labels,
+                    labels: _labels.concat(_forecast_labels),
                     datasets: [{
-                        label: 'Data Aktual',
+                        label: 'Revenue',
                         data: _data,
                         backgroundColor: 'black',
                         borderColor: 'blue',
                         borderWidth: 1
+                    },{
+                        labels:_forecast_labels,
+                        label: 'Forecast',
+                        data: Array(_data.length).fill(null).concat(_forecast_data),
+                        backgroundColor: 'orange',
+                        borderColor: 'white',
+                        borderWidth: 1 
                     }]
                 },
                 options: {
