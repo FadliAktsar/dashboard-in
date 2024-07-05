a2e1130c1b7a84385d328f3ac64793376a21e4c8
Fadli Aktsar <muhammadfadliaktsar@gmail.com>
2024-07-05 15:08:13 +0700

    add id column

--- app/models/peramalan.py
+++ app/models/peramalan.py
@@ -2,6 +2,7 @@ from app.extension import db
 
 class Peramalan(db.Model):
     __tablename__ = 'peramalan'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     Settelement_Date = db.Column(db.Date, nullable=False)
     Forecast = db.Column(db.Integer, nullable=False)
     Revenue = db.Column(db.Integer, nullable=False)
