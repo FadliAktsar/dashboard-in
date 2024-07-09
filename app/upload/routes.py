916fec15a9779c4c24b601f7bb5f4124b66b93cd
Fadli Aktsar <muhammadfadliaktsar@gmail.com>
2024-07-09 13:54:40 +0700

    delete import csv

--- app/upload/routes.py
+++ app/upload/routes.py
@@ -1,12 +1,12 @@
-from flask import render_template, request, redirect, flash, url_for,jsonify
+from flask import render_template, request, jsonify
 
 from app.extension import db
 from app.upload import bp
 from app.models.transaksi import Transaksi
+from sqlalchemy import or_,func, cast, String
 
 from io import StringIO
 import pandas as pd
-import csv
 
 ALLOWED_EXTENSIONS = {'csv'}
 
@@ -19,22 +19,43 @@ def index():
     
     try:
         #get transaction data from db and make it pagination
+        search_query = request.args.get('search', '', type=str)
         page = request.args.get('page', 1, type=int)
-        paginate = db.paginate(db.select(Transaksi)\
-                               .order_by(Transaksi.Settlement_Date.desc()),
-              page=page,
-                per_page=10,
-                  error_out=False)
+
+        query = db.session.query(Transaksi).order_by(Transaksi.Settlement_Date.desc())
+
+        if search_query:
+            search_filter = or_(
+                Transaksi.Transaction_Id.ilike(f'%{search_query}%'),
+                cast(Transaksi.Amount, String).ilike(f'%{search_query}%'),
+                func.to_char(
+                    Transaksi.Settlement_Date, 'YYYY-MM-DD'
+                    ).ilike(f'%{search_query}%'),
+                func.to_char(
+                 Transaksi.Payment_Date, 'YYYY-MM-DD'  
+                ).ilike(f'%{search_query}%'),
+                Transaksi.Payment_Type.ilike(f'%{search_query}%')
+            )
+            query = query.filter(search_filter)
+        
+        paginate = db.paginate(query, page=page, per_page=10, error_out=False)
         
     except Exception as e:
-        error_text = "<p>The error:<br>" + str(e) + "</p>"
-        hed = '<h1>Something is broken.</h1>'
-        return hed + error_text
+        error_text = "The error:" + str(e)
+        hed = 'Something is broken.'
+        return hed + "/" + error_text
+    
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        return jsonify({
+            "table": render_template('upload/_transaction_table.html', transaksi=paginate.items),
+            "pagination": render_template('upload/_pagination.html', pagination=paginate, search_query=search_query)
+        })
 
     return render_template('upload/index.html', 
                            transaksi=paginate,
                            items=paginate.items,
-                           pagination=paginate)
+                           pagination=paginate,
+                           search_query=search_query)
 
 @bp.route('/upload_files', methods=['POST'])
 def upload_files():
@@ -51,48 +72,57 @@ def upload_files():
 
     try:
         stream = StringIO(csv_file.stream.read().decode("UTF8"), newline=None)
-        csv_input = pd.read_csv(stream, delimiter=';', header=None)
+        csv_input = pd.read_csv(stream, delimiter=';', header=0)
 
         required_columns = [
-            'Outlet_Name', 'Merchant_Id', 'Feature', 'Order_Id', 'Transaction_Id', 'Amount', 'Net_Amount',
-            'Transaction_Status', 'Transaction_Time', 'Payment_Type', 'Payment_Date', 'GoPay_Transaction_Id',
-            'GoPay_Reference_Id', 'GoPay_Customer_Id', 'Qris_Transaction_Type', 'Qris_Reference_Id', 'Qris_Issuer',
-            'Qris_Acquirer', 'Card_Type', 'Credit_Card_Number', 'Settlement_Date', 'Settlement_Time'
+            'Outlet name', 'Merchant ID', 'Feature', 'Order ID', 'Transaction ID', 'Amount', 'Net Amount',
+            'Transaction Status', 'Transaction time', 'Payment Type', 'Payment Date', 'GO-PAY Transactions ID',
+            'Gopay Reference Id', 'GoPay Customer ID', 'QRIS Transaction Type', 'QRIS Reference ID', 'QRIS Issuer',
+            'QRIS Acquirer', 'Card Type', 'Credit Card Number', 'Settlement Date', 'Settlement time'
         ]
 
         missing_columns = [col for col in required_columns if col not in csv_input.columns]
         if missing_columns:
-            return jsonify({"message": f"Missing required columns: {', '.join(missing_columns)}", "success": False})
-
+            return jsonify({"message": f"Missing required columns: {', '.join(missing_columns)}",
+                             "success": False})
+        
         # Use transaction to ensure atomicity
         with db.session.begin():
             for _, row in csv_input.iterrows():
-                upload = Transaksi(
-                    Outlet_Name=row['Outlet_Name'],
-                    Merchant_Id=row['Merchant_Id'],
-                    Feature=row['Feature'],
-                    Order_Id=row['Order_Id'],
-                    Transaction_Id=row['Transaction_Id'],
-                    Amount=row['Amount'],
-                    Net_Amount=row['Net_Amount'],
-                    Transaction_Status=row['Transaction_Status'],
-                    Transaction_Time=row['Transaction_Time'],
-                    Payment_Type=row['Payment_Type'],
-                    Payment_Date=row['Payment_Date'],
-                    GoPay_Transaction_Id=row['GoPay_Transaction_Id'],
-                    GoPay_Reference_Id=row['GoPay_Reference_Id'],
-                    GoPay_Customer_Id=row['GoPay_Customer_Id'],
-                    Qris_Transaction_Type=row['Qris_Transaction_Type'],
-                    Qris_Reference_Id=row['Qris_Reference_Id'],
-                    Qris_Issuer=row['Qris_Issuer'],
-                    Qris_Acquirer=row['Qris_Acquirer'],
-                    Card_Type=row['Card_Type'],
-                    Credit_Card_Number=row['Credit_Card_Number'],
-                    Settlement_Date=row['Settlement_Date'],
-                    Settlement_Time=row['Settlement_Time']
-                )
-                db.session.add(upload)
 
+                existing_transaction = Transaksi.query.filter_by(Transaction_Id=row['Transaction ID']).first()
+
+                if existing_transaction:
+                     return jsonify({"message": "Data already exist", "success": False})
+                
+                new_transaction = Transaksi(
+                    Outlet_Name=row['Outlet name'],
+                    Merchant_Id=row['Merchant ID'],
+                    Feature=row['Feature'],
+                    Order_Id=row['Order ID'],
+                    Transaction_Id=row['Transaction ID'],
+                    Amount=row['Amount'],
+                    Net_Amount=row['Net Amount'],
+                    Transaction_Status=row['Transaction Status'],
+                    Transaction_Time=row['Transaction time'],
+                    Payment_Type=row['Payment Type'],
+                    Payment_Date=row['Payment Date'],
+                    GoPay_Transaction_Id=row['GO-PAY Transactions ID'],
+                    GoPay_Reference_Id=row['Gopay Reference Id'],
+                    GoPay_Customer_Id=row['GoPay Customer ID'],
+                    Qris_Transaction_Type=row['QRIS Transaction Type'],
+                    Qris_Reference_Id=row['QRIS Reference ID'],
+                    Qris_Issuer=row['QRIS Issuer'],
+                    Qris_Acquirer=row['QRIS Acquirer'],
+                    Card_Type=row['Card Type'],
+                    Credit_Card_Number=row['Credit Card Number'],
+                    Settlement_Date=row['Settlement Date'],
+                    Settlement_Time=row['Settlement time']
+                )
+
+                db.session.add(new_transaction)
+
+            db.session.commit()  # Komit setelah loop selesai
         return jsonify({"message": "File successfully uploaded and data saved to database", "success": True})
     
     except Exception as e:
