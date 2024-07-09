7339f35a42959ee18664bec0afa30aa6b8e6036c
Fadli Aktsar <muhammadfadliaktsar@gmail.com>
2024-07-09 13:53:00 +0700

    main page endpoint

--- app/main/routes.py
+++ app/main/routes.py
@@ -7,6 +7,6 @@ from app.main import bp
 @bp.route('/')
 #@login_required
 def index():
-    return render_template('index.html',
+    return render_template('main/index.html',
                             #name=current_user.username
                             )
