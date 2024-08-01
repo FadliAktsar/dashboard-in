39c795373c5d5d7198f7c57fed5b1f3cf0b2e142
Fadli Aktsar <muhammadfadliaktsar@gmail.com>
2024-08-01 13:02:30 +0700

    clean up mess

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,12 +1,6 @@
 from flask import render_template
-#from flask_login import login_required, current_user
-
-#from app.model.user import User
 from app.main import bp
 
 @bp.route('/')
-#@login_required
 def index():
-    return render_template('main/index.html',
-                            #name=current_user.username
-                            )
+    return render_template('main/index.html')
