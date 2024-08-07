f13c0f0904983c8ad6f12dd13d1fe1d41aa86cdd
Fadli Aktsar <muhammadfadliaktsar@gmail.com>
2024-08-07 13:23:22 +0700

    .

--- config.py
+++ config.py
@@ -12,18 +12,3 @@ class Config:
         raise ValueError("No SQLALCHEMY_DATABASE_URI set for Flask application.")
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-'''
-class ProductionConfig(Config):
-    DEBUG = False
-
-class StagingConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-class DevelopmentConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-class TestingConfig(Config):
-    TESTING = True
-'''
